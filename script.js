10a27b0d6c03a8c2de7b3361c2dc599589c5242a
Phontakorn Siriapiphatamorn <phontakornsiriapiphatamorn@MacBook-Air-khxng-Phontakorn.local>
2025-07-26 10:34:40 +0700

    Add Function and Hover Button

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 function showMessage() {
     alert("Tracking is Delivered");
 }
+
+function signUpMessage() {
+    alert("On progress");
+}
+
+function signInMessage() {
+    alert("On progress");
+}
